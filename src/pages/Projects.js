474594f54dedb6aecdf7effb4f47f4f5ab26669f
Martypose <martinpose@hotmail.com>
2023-04-02 01:40:51 +0200

    Foto proyecto

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import fotocarnet from '../assets/fotocarnet.png';
 import Navbar from './Navbar';
+import esquema_raspOCV from '../assets/esquema_raspOCV.png';
 
 const projectsData = [
   {
@@ -14,17 +15,18 @@ const projectsData = [
 const Projects = () => {
   return (
     <div className="content-container">
-    <div className="projects">
-      <ul>
-        {projectsData.map((project, index) => (
-          <li key={index}>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
-            <a href={project.repoLink} target="_blank" rel="noopener noreferrer">Repositorio en GitHub</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="projects">
+        <ul>
+          {projectsData.map((project, index) => (
+            <li key={index}>
+              <h3>{project.name}</h3>
+              <p>{project.description}</p>
+              <a href={project.repoLink} target="_blank" rel="noopener noreferrer">Repositorio en GitHub</a>
+            </li>
+          ))}
+        </ul>
+        <img src={esquema_raspOCV} alt="Esquema Raspberry Pi y OpenCV" className="project-image" />
+      </div>
     </div>
   );
 };
