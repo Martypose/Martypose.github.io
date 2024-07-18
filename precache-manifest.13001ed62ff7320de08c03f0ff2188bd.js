271b2530ea9923d694668a76ab8f530bb984756c
Martypose <martinpose@hotmail.com>
2024-07-18 18:06:34 +0200

    Updates

---
+++ precache-manifest.13001ed62ff7320de08c03f0ff2188bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e751e671411d66096216f60fe3dfb11a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63b48da0e5ecfe58147f",
+    "url": "/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "8b7469f8df9b767709dd",
+    "url": "/static/css/main.32bfd096.chunk.css"
+  },
+  {
+    "revision": "63b48da0e5ecfe58147f",
+    "url": "/static/js/2.8275280b.chunk.js"
+  },
+  {
+    "revision": "0424fba02e3bf148ea07",
+    "url": "/static/js/3.df7899f0.chunk.js"
+  },
+  {
+    "revision": "8b7469f8df9b767709dd",
+    "url": "/static/js/main.e20ab548.chunk.js"
+  },
+  {
+    "revision": "6fa435f97d2b16ad2936",
+    "url": "/static/js/runtime~main.5fd0fd28.js"
+  },
+  {
+    "revision": "95b956b8536bce13a29f3bdf517bbdb7",
+    "url": "/static/media/Flutter.95b956b8.png"
+  },
+  {
+    "revision": "85f9555c5e7f880b41dc0508a3ce9d53",
+    "url": "/static/media/Martín_Pose_-_Software_Developer.85f9555c.pdf"
+  },
+  {
+    "revision": "04e84eb4249e73ba6bf9c40bf9e1869b",
+    "url": "/static/media/analislog.04e84eb4.png"
+  },
+  {
+    "revision": "f4e8a8d2abe4cf7ce3aec1b05375bce1",
+    "url": "/static/media/api.f4e8a8d2.png"
+  },
+  {
+    "revision": "afd0947f40df95bd7dad4be83ed9121e",
+    "url": "/static/media/dark.afd0947f.png"
+  },
+  {
+    "revision": "6045942659d7248b2382b86d4836ea9b",
+    "url": "/static/media/esquema_raspOCV.60459426.png"
+  },
+  {
+    "revision": "9750f7976a7c381f744073e8dc0f10cb",
+    "url": "/static/media/git.9750f797.jpg"
+  },
+  {
+    "revision": "5e5e0b5962891b5797d754101332c831",
+    "url": "/static/media/light.5e5e0b59.png"
+  },
+  {
+    "revision": "9f26c666bb898eac2e0d849df06efb19",
+    "url": "/static/media/martin.9f26c666.png"
+  },
+  {
+    "revision": "26da64f5aab5b353ff6f7fb273e0413a",
+    "url": "/static/media/react_app.26da64f5.png"
+  }
+]);
